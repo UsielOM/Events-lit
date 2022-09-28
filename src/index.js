d0fa855ebfe17ab38d86915176fdaf76007c333b
UsielOM <usiel31197@outlook.es>
2022-09-28 11:52:46 -0500

    errores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import './ejemplo-event1';
 import './ejemplo-event2';
 import './ejemplo-event3';
-import './ejemplo-archivo4';
-import './ejemplo-event5Dispatcher';
-import './ejemplo-event5Listener';
